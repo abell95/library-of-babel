abbeb80755dbb678e2f5001be6518308a7a71a13
Andrew Bell <belland@oregonstate.edu>
2020-01-01 02:56:38 -0800

    Add basic seeding and rng. TODO: Better organize JS please.

--- babel.js
+++ babel.js
@@ -2,8 +2,33 @@ const validChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
     'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
     'v', 'w', 'x', 'y', 'z', ',', '.', ' '];
 
+String.prototype.hashCode = function () {
+    var hash = 0, i, chr;
+    if (this.length === 0) return hash;
+    for (i = 0; i < this.length; i++) {
+        chr = this.charCodeAt(i);
+        hash = ((hash << 5) - hash) + chr;
+        hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+};
+
+let next = Math.random() * 5;
+
+const rand = () => {
+  next = next * 1103515245 + 12345;
+  let ret = ((next/65536) % (2 ** 31 - 1)) % validChars.length;
+  next = next % 1103515249;
+  return ret;
+}
+
+const srand = seed => {
+  next = seed;
+}
+
 const randomChar = () => {
-    let charIndex = Math.floor(Math.random() * Math.floor(validChars.length));
+    let charIndex = Math.floor(rand());
+    console.log(next);
     return validChars[charIndex];
 }
 
@@ -31,6 +56,12 @@ const generatePage = () => {
 }
 
 window.onload = () => {
+    hexstring = window.prompt("Enter seed or do nothing");
+    if (hexstring) {
+        let seednum = hexstring.hashCode();
+        if (seednum < 0) seednum = -seednum;
+        srand(seednum);
+    }
     generateTitle();
     generatePage();
 }
